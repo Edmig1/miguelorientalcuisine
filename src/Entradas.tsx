0c318985a8657ff9fef081d09e6fa7fa35130e11
Edmig1 <miguelitoplayer1@gmail.com>
2026-05-30 04:29:48 -0300

    imagens

--- src/Entradas.tsx
+++ src/Entradas.tsx
@@ -10,7 +10,7 @@ function Entradas({pedido,setPedido}) {
             <div className={` w-[80%] flex justify-self-start items-center py-5 mb-5 border-b-2 border-red-600`}>
                 <h2 className={"text-4xl"}>Entradas</h2>
             </div>
-            <CardProduto pedido={pedido} setPedido={setPedido} src={"/guioza.jpg"} titulo={"Guioza"} desc={"Delicados pastéis orientais recheados com carne suculenta e legumes frescos, envoltos em uma fina massa artesanal. Grelhados até ficarem dourados e levemente crocantes por fora, mantendo um recheio macio e saboroso por dentro. Servidos com molho especial à base de shoyu para uma experiência autêntica da culinária japonesa."}></CardProduto>
+            <CardProduto pedido={pedido} setPedido={setPedido} src={`${process.env.PUBLIC_URL}/guioza.jpg`} titulo={"Guioza"} desc={"Delicados pastéis orientais recheados com carne suculenta e legumes frescos, envoltos em uma fina massa artesanal. Grelhados até ficarem dourados e levemente crocantes por fora, mantendo um recheio macio e saboroso por dentro. Servidos com molho especial à base de shoyu para uma experiência autêntica da culinária japonesa."}></CardProduto>
         </div>
     );
 }
--- src/Sobremesas.tsx
+++ src/Sobremesas.tsx
@@ -10,7 +10,7 @@ function Sobremesas({pedido,setPedido}) {
             <div className={` w-[80%] flex justify-self-start items-center py-5 mb-5 border-b-2 border-red-600`}>
                 <h2 className={"text-4xl"}>Sobremesas</h2>
             </div>
-            <CardProduto pedido={pedido} setPedido={setPedido} src={"/roll1.png"} titulo={"Roll Crocante de Nutella"} desc={"Uma deliciosa panqueca fina de chocolate, cuidadosamente enrolada para envolver um recheio irresistível de Nutella cremosa com flocos de milho crocantes, proporcionando uma combinação perfeita entre suavidade e textura a cada mordida. Cada fatia é finalizada com uma rodela fresca de morango, que traz um toque levemente ácido e refrescante, equilibrando a intensidade do chocolate.\n" +
+            <CardProduto pedido={pedido} setPedido={setPedido} src={`${process.env.PUBLIC_URL}/roll1.png`} titulo={"Roll Crocante de Nutella"} desc={"Uma deliciosa panqueca fina de chocolate, cuidadosamente enrolada para envolver um recheio irresistível de Nutella cremosa com flocos de milho crocantes, proporcionando uma combinação perfeita entre suavidade e textura a cada mordida. Cada fatia é finalizada com uma rodela fresca de morango, que traz um toque levemente ácido e refrescante, equilibrando a intensidade do chocolate.\n" +
                 "\n" +
                 "Para tornar a experiência ainda mais especial, o roll recebe um generoso fio de Nutella por cima e uma cobertura de Ovomaltine polvilhado, adicionando camadas extras de sabor e uma crocância marcante. Uma sobremesa surpreendente, feita para quem não abre mão de uma explosão de chocolate, cremosidade e contraste de texturas em cada pedaço."}></CardProduto>
         </div>
